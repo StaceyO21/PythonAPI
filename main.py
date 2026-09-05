93f91603a6dff929b585e978051cc40e2ae1b1da
StaceyO21 <staceyobrien21@gmail.com>
2026-09-05 18:18:30 -0500

    adding validation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email import message
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
@@ -36,16 +37,28 @@ class MyAPI(BaseHTTPRequestHandler):
             body = self.rfile.read(content_length)
             data = json.loads(body)
 
-            response = {
-                "you_sent": data
-            }
+            if 'message' not in data:
+                self.send_response(400)
+
+                response = {
+                    "error": "Message is required"
+                }
+
+            else:
+                message = data['message']
+
+                self.send_response(200)
+
+                response = {
+                    "reply": f"You said: {message}"
+                }
 
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
 
 
-        self.wfile.write(json.dumps(response).encode())
+            self.wfile.write(json.dumps(response).encode())
 
 server = HTTPServer(('localhost', 8080), MyAPI)
 
